9c3c679a9edaea622620dea07f650aff129d06a7
Olli Mannevaara <ommann@utu.fi>
2016-12-07 00:03:13 +0200

    Polished the example code.

--- main.lua
+++ main.lua
@@ -2,7 +2,7 @@ curve = require 'curvy'
 
 love.load = function()
   --From 0 to 100 it takes 0.8 seconds
-  acceleration = curve( {0, 0.8, 2.20}, {0, 100, 200, 999} )
+  acceleration = curve( {0, 0.8, 2.20}, {0, 100, 200} )
 
   --From the speed of 25 we further accelerate for a second
   speed1 = acceleration(25, 1) --~128.6
